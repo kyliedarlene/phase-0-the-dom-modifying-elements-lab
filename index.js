e2e7143d9137f1b25e50dc520202802edb67f455
Kylie Clark <kyliedarlene@icloud.com>
2023-12-02 00:55:17 -0700

    not sure why tests are failing

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+const newId = document.createAttribute("id");
+newId.value = "victory";
+newHeader.setAttributeNode(newId);
+
+newHeader.textContent = "Beck is the champion!";

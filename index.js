3f1be0ccfa2ef96734a2047b0dae2e0a52c4746e
Kylie Clark <kyliedarlene@icloud.com>
2023-12-02 11:28:52 -0700

    complete lab

--- index.js
+++ index.js
@@ -3,10 +3,16 @@
 main.remove();
 
 const newHeader = document.createElement("h1");
-document.body.append(newHeader);
+// document.body.append(newHeader);
+
+newHeader.id = "victory";
+
+/*
 
 const newId = document.createAttribute("id");
 newId.value = "victory";
 newHeader.setAttributeNode(newId);
+
+*/
 
 newHeader.textContent = "Beck is the champion!";
